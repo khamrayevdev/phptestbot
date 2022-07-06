70ea87a950d72e5edcf0fe75ac31e63771d91dd0
Jorabek Khamrayev <104376503+khamrayevdev@users.noreply.github.com>
2022-07-06 23:18:52 +0500

    Create connect.php

---
+++ connect.php
@@ -0,0 +1,7 @@
+<?php
+define("DB_SERVER","localhost");
+define("DB_USERNAME","baxt_uz");//mysql login
+define("DB_PASSWORD","baza");//mysql parol
+define("DB_NAME","baxt_uz");//mysql login
+$connect = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_NAME);
+?>
